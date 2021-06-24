c930a551df88f597d96d83704d10c4d50e861769
oguzhangur <oguzhan_gur@outlook.com>
2021-06-24 19:56:13 +0300

    WIP plotter

--- plot_results.py
+++ plot_results.py
@@ -4,12 +4,16 @@ import pandas as pd
 import glob
 from plotter import Plotter
 
-log_dir = "COMPLEX_MOV_RESULTS/*"
+log_dir = "COMPLEX_MOV_RESULTS/"
 df_dict = {
-    "DRQN": [pd.DataFrame({"episodes": range(len(np.load(file))), "reward": np.load(file)}) for file in glob.glob(log_dir) if file.endswith("eval_Mario_DRQN.npy")],
-    "DARQN": [pd.DataFrame({"episodes": range(len(np.load(file))), "reward": np.load(file)}) for file in glob.glob(log_dir) if file.endswith("eval_Mario_DARQN.npy")],
-    "DCBAMRQN": [pd.DataFrame({"episodes": range(len(np.load(file))), "reward": np.load(file)}) for file in glob.glob(log_dir) if file.endswith("eval_Mario_DCBAMRQN.npy")]
+    "DRQN": [pd.DataFrame({"episodes": range(len(np.load(f"{log_dir}eval_Mario_DRQN.npy"))), "reward": np.load(f"{log_dir}eval_Mario_DRQN.npy")})],
+    "DARQN": [pd.DataFrame({"episodes": range(len(np.load(f"{log_dir}eval_Mario_DARQN.npy"))),
+                           "reward": np.load(f"{log_dir}eval_Mario_DARQN.npy")})],
+    "DCBAMRQN": [pd.DataFrame({"episodes": range(len(np.load(f"{log_dir}eval_Mario_DCBAMRQN.npy"))),
+                           "reward": np.load(f"{log_dir}eval_Mario_DCBAMRQN.npy")})],
 }
+
+print(df_dict["DCBAMRQN"][0])
 
 pltter = Plotter(df_dict)
 pltter()
