9715cf5a0e2735e0c81fd65091877dd2d2965261
oguzhangur <oguzhan_gur@outlook.com>
2021-06-24 19:58:04 +0300

    update train drqn with improvements

--- Train_DRQN.py
+++ Train_DRQN.py
@@ -3,26 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 import time
 import random
+import pickle
 import numpy as np
 from statistics import mean
 from Buffer import ReplayBuffer
-from Environment import CreateMario
+from Environment import CreateBreakout
 from Network import QNet_LSTM
 
 # settings
-Train_max_step = 2000000
-learning_rate = 2e-4
-gamma = 0.99
-buffer_capacity = 250000
-batch_size = 32
-replay_start_size = 50000
+Train_max_step         = 4000000
+learning_rate          = 3e-4
+gamma                  = 0.99
+buffer_capacity        = 250000
+batch_size             = 32
+replay_start_size      = 50000
 final_exploration_step = 1000000
-update_interval = 10000  # target net
-update_frequency = 8  # the number of actions selected by the agent between successive SGD updates
-save_interval = 10000
-model_path = './Models/Mario_DRQN.model'
-history_path = './Train_Historys/Mario_DRQN'
-eval_history_path = './Train_Historys/eval_Mario_DRQN'
+update_interval        = 10000 # target net
+update_frequency       = 4  # the number of actions selected by the agent between successive SGD updates
+save_interval          = 10000
+model_path = './Models/Breakout_DARQN.model'
+buffer_path = './Models/Breakout_DARQN.buffer'
+history_path = './Train_Historys/Breakout_DARQN'
+eval_history_path = './Train_Historys/eval_Breakout_DARQN'
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
@@ -31,7 +33,7 @@ print(device)
 
 
 def init_hidden():
-    h, c = torch.zeros([1, 512], dtype=torch.float).to(device), torch.zeros([1, 512], dtype=torch.float).to(device)
+    h, c = torch.zeros([1, 64], dtype=torch.float).to(device), torch.zeros([1, 64], dtype=torch.float).to(device)
     return h, c
 
 
@@ -75,7 +77,7 @@ def train(optimizer, behaviourNet, targetNet, s_batch, a_batch, r_batch, done_ba
 
 
 def main():
-    env = CreateMario(stack=False)
+    env = CreateBreakout(stack=False)
     buffer = ReplayBuffer(buffer_capacity)
     behaviourNet = QNet_LSTM().to(device)
     # behaviourNet.load_state_dict(torch.load(model_path))
@@ -98,20 +100,18 @@ def main():
     while step < Train_max_step:
         # env.render()
         epsilon = max(0.1, 1.0 - (0.9 / final_exploration_step) * step)
-
         action_value, (next_h, next_c) = behaviourNet.forward(torch.FloatTensor([state]).to(device), (h, c))
 
         # epsilon greedy
         coin = random.random()
         if coin < epsilon:
-            action = random.randrange(5)
+            action = random.randrange(4)
         else:
             action = action_value.argmax().item()
-
         next_state, reward, done, info = env.step(action)
-        buffer.push((state, action, info['score'], 1 - done))
+        buffer.push((state, action, reward, 1 - done))
 
-        score += info['score']
+        score += reward
         step += 1
 
         if done:
@@ -143,8 +143,8 @@ def main():
                 # take action get next state, rewards and terminal status
                 action_value, (next_h, next_c) = behaviourNet.forward(torch.FloatTensor([state]).to(device), (h, c))
                 state, reward, done, info = env.step(action_value.argmax().item())
-                episodic_reward = episodic_reward + info['score']
-                h, c = (next_h, next_c)
+                episodic_reward = episodic_reward + reward
+                h, c = next_h, next_c
             h, c = init_hidden()
             state = env.reset()
             score = 0
@@ -153,7 +153,8 @@ def main():
             eval_history.append(episodic_reward)
 
             torch.save(behaviourNet.state_dict(), model_path)
-
+            with open(buffer_path, 'wb') as f:
+                pickle.dump(buffer, f)
             np.save(history_path, np.array(train_history))
             np.save(eval_history_path, np.array(eval_history))
 
@@ -161,13 +162,15 @@ def main():
             print(
                 f"Step No: {step}, Train average: {mean(score_history)}, Eval Average: {episodic_reward}, epsilon: {epsilon}, time = {end - start} ")
             start = end
+    with open(buffer_path, 'wb') as f:
+        pickle.dump(buffer, f)
     torch.save(behaviourNet.state_dict(), model_path)
     np.save(history_path, np.array(train_history))
     np.save(eval_history_path, np.array(eval_history))
     print("Train end, avg_score of last 100 episode : {}".format(mean(score_history)))
 
 def main_v2():
-    env = CreateMario(stack=False)
+    env = CreateBreakout(stack=False)
     behaviourNet = QNet_LSTM().to(device)
     print(sum(p.numel() for p in behaviourNet.parameters() if p.requires_grad))
     behaviourNet.load_state_dict(torch.load(model_path))
@@ -183,9 +186,11 @@ def main_v2():
         episodic_reward = episodic_reward + reward
         h, c = next_h, next_c
         env.render()
+        # print(h)
         time.sleep(1/10)
 
     print(episodic_reward)
+
 
 if __name__ == "__main__":
     main()
